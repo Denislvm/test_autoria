2f488b9062702e5bac31301fc08cedd5f2c57f83
Denis <62796570+Denislvm@users.noreply.github.com>
2023-06-27 17:34:39 +0300

    Update main.py

--- main.py
+++ main.py
@@ -14,21 +14,49 @@ def get_soup(url):
     return BeautifulSoup(response.text, 'html.parser')
 
 
-def parse_process():
-    while True:
-        page = get_soup(main_url)
-        all_cars = page.findAll('div', _class='content')
-        for car in all_cars:
-            link = car.find('a', _class='js-newAutoInformerLink address')
+# url2 = "https://auto.ria.com/uk/search/?categories.main.id=1&brand.id[0]=79&model.id[0]=2104&indexName=auto,order_auto,newauto_search&country.import.usa.not=0&damage.not=0&page=1"
+def main():
+    current_page = 1
+    last_page = None
+    current_url = main_url
 
-            print(link)
+    while True:
+
+        # url = f"https://auto.ria.com/uk/search/?categories.main.id=1&brand.id[0]=79&model.id[0]=2104&indexName=auto,order_auto,newauto_search&country.import.usa.not=0&damage.not=0&page={current_page}"
+        soup = get_soup(current_url)
+        all_cars = soup.findAll('div', class_='content')
+        for car in all_cars:
+            link = car.find('a').get('href')
+            # print(link)
+            sub_car = get_soup(link)
+            car_page = sub_car.findAll('div', class_='ticket-status-0')
+            for car1 in car_page:
+                mark = car1.find('h1', class_='head').text
+                price = car1.find('div', class_='price_value').text
+                mileage = car1.find('div', class_='base-information bold').text
+                place = car1.find('div', class_='item_inner').text
+
+                print(link, '\n', mark, '\n', price, '\n', mileage, '\n', place, '\n')
+
+        if last_page is None:
+            last_page_element = soup.find('span', class_='page-item.last')
+            if last_page_element is not None:
+                last_page = int(last_page_element.find('a').text)
+
+        if current_url == main_url:
+            current_url = f"{main_url}&page={current_page}"
+        elif current_page == last_page:
+            print()
+            # Достигнута последняя страница
+            break
+        else:
+            current_url = f"{main_url}&page={current_page}"
+
+        current_page += 1
 
         # when project will over, this parametr will be 600 millisec
         # time.sleep(600)
 
 
-n = parse_process()
-print(n)
-
-
-
+if __name__ == "__main__":
+    main()
